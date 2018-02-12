158e72844191096bee2cb33ad11d5a7b4b0e029a
nikola <36268035+npilipovic86@users.noreply.github.com>
2018-02-12 09:55:14 +0100

    Update db-init.sql

--- Server/sql/db-init.sql
+++ Server/sql/db-init.sql
@@ -1,7 +1,7 @@
 create database cf;
 
 
--- pass je admin
+-- password  admin
 insert into security_user (username, password, first_name, last_name, role) values 
 	('admin', '$2a$10$bMAvk.OlEW/ZKdidfl6dKO9/ZVOtvpTiNZeMJF6oPmzpb9dZWgbd.', 'Admin', 'Admin', 'ADMINISTRATOR');
     
